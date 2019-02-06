ad2aaeb01d51f65f07e44fc14ba13593dacf9f4f
Sergey Shapirenko <s.shapirenko@hsdigital.ru>
2019-02-06 17:59:17 +0700

    checking hash

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+// @ts-ignore
+import md5 from 'md5';
 
-const App = () => (
-    <h1>test</h1>
-);
+import { CHECKSUM } from '../config';
+
+class App extends Component<any, any> {
+    constructor(props: any) {
+        super(props);
+    }
+    
+    async componentWillMount() {
+        const { items }= await require('../data/10_items.json');
+        const hash = md5(items);
+        const storedHash = localStorage.getItem(CHECKSUM);
+
+        if (storedHash && storedHash !== hash) {
+            // обнуляем стейт
+            localStorage.setItem(CHECKSUM, hash)
+        }
+    }
+
+    render () {
+        return (
+            <h1>app</h1>
+        );
+    }
+};
 
 export default App;
---
+++ src/config.ts
@@ -0,0 +1,1 @@
+export const CHECKSUM: string = 'CHECKSUM';
