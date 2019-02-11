4b2bf037206e714ed00f331b39f61de3ef83eefc
Sergey Shapirenko <sshapirenko@gmail.com>
2019-02-11 08:26:45 +0700

    add history

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-// import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-// import styled from 'styled-components';
+import { Router } from 'react-router-dom';
 
 import { store } from './store';
+import history from './history';
 import App from './components/App';
 import './index.css';
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <Router history={history}>
+            <App />
+        </Router>
     </Provider>,
     document.getElementById('root')
 );
